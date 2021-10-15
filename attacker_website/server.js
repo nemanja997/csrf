69cf4629cedb1b65872b69671a0edcb94f11a9b1
Nemanja Kazic <nemanja.kazic@vast.com>
2021-10-15 14:01:27 +0200

    Add route for generating CSRF token when we have a CSRF secret

--- attacker_website/server.js
+++ attacker_website/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const cookieParser = require('cookie-parser');
+const Tokens = require('csrf');
+
+const tokens = new Tokens();
 
 const PORT_NUMBER = 3000;
 
@@ -10,6 +13,12 @@ app.use(express.json());
 
 app.get('/', function(req, res) {
     res.sendFile(path.resolve(__dirname, './index.html'));
+});
+
+app.get('/generate_csrf/:csrf_secret', function(req, res) {
+    const csrfSecret = req.params.csrf_secret;
+    var token = tokens.create(csrfSecret);
+    res.json({ token })
 });
 
 app.listen(PORT_NUMBER, () => {
